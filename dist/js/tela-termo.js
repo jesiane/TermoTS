e10fb6fecfcfcd52d390b5e7ac40610e73f04cf3
Jesiane Straubel <jesi1951@hotmail.com>
2023-08-25 13:28:09 -0300

    first commit

---
+++ dist/js/tela-termo.js
@@ -0,0 +1,24 @@
+"use strict";
+import {Termo} from "./tela-termo"
+class TelaTermo {
+  constructor(){
+    this.pnlTermo = document.getElementById('pnlTermo');
+    this.pnlTeclado = document.getElementById('pnlTeclado');
+    this.btnEnter = document.getElementById('btnEnter');
+    this.lbNotificacao = document.getElementById('lbNotificacao');
+  }
+  registrarEventos(){
+
+  }
+  atualizarJogo(){
+
+  }
+  AvaliarPalavra(){
+
+  }
+  
+
+
+}
+
+//# sourceMappingURL=tela-termo.js.map
